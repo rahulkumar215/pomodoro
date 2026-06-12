aad7ad4cf13cafb99f4cf44facab491e8af836c8
rahulkumar215 <rk1617015@gmail.com>
2026-06-12 11:35:22 +0530

    feat: add singup, signin and me route.

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -3,6 +3,8 @@ import tasks from "./tasks";
 import projects from "./projects";
 import bcrypt from "bcrypt";
 import { prisma } from "../../db";
+import jwt from "jsonwebtoken";
+import config from "../../config";
 
 const v1: Router = express.Router();
 
@@ -28,6 +30,60 @@ v1.post("/signup", async (req: Request, res: Response) => {
     },
   });
 });
-v1.post("/signin", (req, res) => {});
+v1.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    res.status(400).json({
+      message: "User does not exist.",
+    });
+    return;
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (passwordMatch) {
+    const token = jwt.sign(
+      {
+        email: user.email,
+      },
+      config.jwt_secret,
+    );
+
+    res.send({
+      token,
+    });
+  }
+});
+v1.get("/me", async (req, res) => {
+  const token = req.headers.authorization || "";
+  const userDetails = jwt.verify(token, config.jwt_secret);
+
+  const email = userDetails.email;
+  const user = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+
+  if (user) {
+    res.send({
+      name: user.name,
+      email: user.email,
+      photo: user.photo,
+      isVerified: user.is_verified,
+    });
+  } else {
+    res.status(401).send({
+      message: "Unauthorized",
+    });
+  }
+});
 
 export default v1;
