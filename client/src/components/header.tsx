da4ba4e6b6eff255d32c19774857c5847ca0ce15
rahulkumar215 <rk1617015@gmail.com>
2026-07-17 19:09:06 +0530

    feat: add more controls to the sessions report table

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -114,7 +114,8 @@ export function DataTable({ columns }: DataTableProps<TData, TValue>) {
     pageSize: 10, //default page size
   });
 
-  const { isPending, isError, error, data } = useSessions(pagination);
+  const { isPending, isFetching, isError, error, data } =
+    useSessions(pagination);
 
   const serverPageCount = Math.ceil(
     data?.totalCount ?? 0 / pagination.pageSize,
@@ -194,18 +195,61 @@ export function DataTable({ columns }: DataTableProps<TData, TValue>) {
 
       <div className="flex items-center justify-center  gap-2">
         <Button
+          className="border rounded p-1"
+          onClick={() => table.firstPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<<"}
+        </Button>
+        <Button
           onClick={() => table.previousPage()}
           disabled={!table.getCanPreviousPage()}
         >
           {"<"}
         </Button>
-        <span>{pagination.pageIndex + 1}</span>
+        <span>
+          {table.getState().pagination.pageIndex + 1} of{" "}
+          {table.getPageCount().toLocaleString()}
+        </span>
         <Button
           onClick={() => table.nextPage()}
           disabled={!table.getCanNextPage()}
         >
           {">"}
         </Button>
+        <Button
+          className="border rounded p-1"
+          onClick={() => table.lastPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {">>"}
+        </Button>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            min="1"
+            max={table.getPageCount()}
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              table.setPageIndex(page);
+            }}
+            className="border p-1 rounded w-16"
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );
