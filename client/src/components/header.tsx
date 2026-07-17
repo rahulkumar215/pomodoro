a26964df3de1e03225a1b46e7c3e1cc213a23348
rahulkumar215 <rk1617015@gmail.com>
2026-07-17 23:21:03 +0530

    feat: add theme in the application

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -5,6 +5,8 @@ import {
   CrownIcon,
   EllipsisVerticalIcon,
   LogOut,
+  MoonIcon,
+  SunIcon,
   Trash2Icon,
   UserCircleIcon,
 } from "lucide-react";
@@ -44,6 +46,7 @@ import { handleError } from "@/lib/handleError";
 import { toast } from "sonner";
 import { usePlanDialog } from "@/context/PlamDialogContext";
 import { useAuth } from "@/context/AuthContext";
+import { useTheme } from "@/context/ThemeProvider";
 
 const columnHelper = createColumnHelper<SessionsResponse>();
 
@@ -260,6 +263,7 @@ const Header = () => {
   const { user } = useAuth();
   const { setOpenPlanDialog } = usePlanDialog();
   const navigate = useNavigate();
+  const { setTheme } = useTheme();
 
   const handleLogout = async () => {
     try {
@@ -292,6 +296,27 @@ const Header = () => {
       </h2>
 
       <div className="flex items-center gap-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <SunIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
         <Button onClick={() => setOpen(true)}>
           <ChartColumn />
           Reprot
