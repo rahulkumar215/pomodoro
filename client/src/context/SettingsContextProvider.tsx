1488dc03a6e9a642770d20cca5e167cf04260e82
rahulkumar215 <rk1617015@gmail.com>
2026-07-17 19:00:52 +0530

    feat: localise settings for the not logged in user.

--- client/src/context/SettingsContextProvider.tsx
+++ client/src/context/SettingsContextProvider.tsx
@@ -5,14 +5,21 @@ import { SettingsContext } from "./SettingsContext";
 import { useUpdateSettings } from "@/hooks/useSettingsAPI";
 
 export function SettingsContextProvider({ children }: { children: ReactNode }) {
-  const [settings] = useState<Settings>(() => {
+  const [settings, setSettings] = useState<Settings>(() => {
     const saved = localStorage.getItem("settings");
     return saved !== null ? JSON.parse(saved) : DEFAULT_SETTINGS;
   });
+  const token = localStorage.getItem("token");
 
   const mutate = useUpdateSettings();
 
   function handleSaveSettings(settings: Settings) {
+    if (!token) {
+      setSettings(settings);
+      localStorage.setItem("settings", JSON.stringify(settings));
+      window.location.reload();
+      return;
+    }
     mutate.mutate(settings);
   }
 
